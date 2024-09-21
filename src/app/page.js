bbd2bee9c65b630fe0a511c09a010b3bccaeb6f7
dev_tahir <kalaiwalatahir@gmail.com>
2024-09-21 18:36:03 +0530

    remove loader for now

--- src/app/page.js
+++ src/app/page.js
@@ -4,32 +4,19 @@ import Header from "../../Components/Header";
 import Hero from "../../Components/Hero";
 import MainHeading from "../../Components/MainHeading";
 import ProductCard from "../../Components/ProductCard";
-import LoadingScreen from '../../Components/LoadingScreen';
 
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
- 
-  useEffect(() => {
-    setTimeout(() => setLoading(true), 6000)
-  }, [])
 
 
   return (
     <>
-
-      {loading ? (
-        <>
         <Header />
         <main className="min-h-screen">
           <MainHeading />
           <Hero />
           <ProductCard />
         </main>
-        </>
-      ) : (
-        <LoadingScreen />
-      )}
     </>
   );
 }
